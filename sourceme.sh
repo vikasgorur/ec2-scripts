dea9bd036f5c6df017a88e9bcfb37c6a27977542
Vikas Gorur <vikas@gluster.com>
2011-04-19 14:32:48 -0700

    create/terminate/list/describe instance: Enhance to take owner and key as command-line arguments.

--- sourceme.sh
+++ sourceme.sh
@@ -2,9 +2,6 @@
 
 # Begin user-editable section
 
-# Change this to your name
-export EC2_OWNER="Vikas"
-
 # Name of the SSH key. A corresponding key file should exist in this directory.
 # If the key name is "vikas", the file should be called "vikas.pem"
 export EC2_SSH_KEY="vikas"
